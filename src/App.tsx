492d855cec15cb73ba328e551de555d4021b8b6d
dylanmelhuish <dylan.melhuish.pe@gmail.com>
2024-08-08 00:59:02 +0100

    Add fontawesome icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import { Flex, Panel } from "./Components";
 import styles from "./App.module.scss";
+import { Timers } from "./Pages";
+import { Flex } from "./Components";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCog } from "@fortawesome/free-solid-svg-icons/faCog";
 
 export const App = () => {
     return (
         <div className={styles["app-container"]}>
-            <Panel>
-                <Flex direction={"row"} justify={"space-between"}>
-                    <div>Test</div>
-                    <div>Test</div>
-                </Flex>
-            </Panel>
+            <Flex direction={"row"} justify={"space-between"}>
+                <div>Timer</div>
+                <div>
+                    <FontAwesomeIcon icon={faCog} />
+                </div>
+            </Flex>
+            <Timers />
         </div>
     );
 };
